f42a23d0e7da025ebb113797b2a6822282ff1877
ocholaowuor <ocholaowuor@yahoo.com>
2022-08-03 20:11:32 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from './components/Todo';
+
+function App() {
+  return (
+   <div>
+    <h1>My Todos</h1>
+      <Todo text='Feed Scooby'/>
+      <Todo text='Water My Plants'/>
+      <Todo text='Visit the Optician'/>
+  </div>
+  );
+}
+
+export default App;
